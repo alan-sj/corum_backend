140c4f301f9321c8964bc6a3b42a038b0345c7aa
nearHaan <farhaan.nizam675@gmail.com>
2025-09-26 10:02:37 +0530

    bug fix

--- src/routes/answerRoutes.ts
+++ src/routes/answerRoutes.ts
@@ -1,26 +1,26 @@
-import {Router} from 'express';
+import { Router } from "express";
 
-import Answer from '../models/Answer';
+import Answer from "../models/Answer.js";
 
-const router= Router();
+const router = Router();
 
-router.post("/", async (req,res)=>{
-    try{
-        const answer=new Answer(req.body);
-        const savedAnswer=await answer.save();
-        res.status(201).json(savedAnswer);
-    }   catch(err:any){
-        res.status(400).json({error: err.message});
-        }
+router.post("/", async (req, res) => {
+  try {
+    const answer = new Answer(req.body);
+    const savedAnswer = await answer.save();
+    res.status(201).json(savedAnswer);
+  } catch (err: any) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
-router.get("/",async(req,res)=>{
-    try{
-        const answer=await Answer.find();
-        res.json(answer);
-    } catch(err:any){
-        res.status(500).json({error: err.message});
-    }
+router.get("/", async (req, res) => {
+  try {
+    const answer = await Answer.find();
+    res.json(answer);
+  } catch (err: any) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 export default router;
